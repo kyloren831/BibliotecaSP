5064e6c9cf374c44491909f201646577d23a8457
kyloren831 <aaronmc080301@hotmail.com>
2025-06-15 15:29:00 -0600

    login

--- BibliotecaSP/Login.Designer.cs
+++ BibliotecaSP/Login.Designer.cs
@@ -29,12 +29,22 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            groupBox1 = new GroupBox();
             pictureBox1 = new PictureBox();
+            groupBox1 = new GroupBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
             label1 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            txtCorreo = new TextBox();
+            txtPass = new TextBox();
+            button1 = new Button();
+            btnLogin = new Button();
             panel1.SuspendLayout();
-            groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -47,17 +57,6 @@
             panel1.Size = new Size(334, 452);
             panel1.TabIndex = 0;
             // 
-            // groupBox1
-            // 
-            groupBox1.Controls.Add(label1);
-            groupBox1.Font = new Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            groupBox1.Location = new Point(38, 154);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(265, 225);
-            groupBox1.TabIndex = 1;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "Bliblioteca SP";
-            // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.biblioteca_en_linea__1_;
@@ -67,6 +66,50 @@
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 2;
             pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Palatino Linotype", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(38, 154);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(265, 225);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Bliblioteca SP";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Palatino Linotype", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(56, 150);
+            label4.Name = "label4";
+            label4.Size = new Size(131, 18);
+            label4.TabIndex = 3;
+            label4.Text = "Control de permisos";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Palatino Linotype", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(56, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(135, 18);
+            label3.TabIndex = 2;
+            label3.Text = "Registro de Pantallas";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Palatino Linotype", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(56, 79);
+            label2.Name = "label2";
+            label2.Size = new Size(134, 18);
+            label2.TabIndex = 1;
+            label2.Text = "Registro de Sistemas";
             // 
             // label1
             // 
@@ -78,20 +121,97 @@
             label1.TabIndex = 0;
             label1.Text = "Registro de Usuarios";
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Palatino Linotype", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(504, 96);
+            label5.Name = "label5";
+            label5.Size = new Size(163, 32);
+            label5.TabIndex = 4;
+            label5.Text = "Iniciar Sesion";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Palatino Linotype", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(443, 153);
+            label6.Name = "label6";
+            label6.Size = new Size(54, 18);
+            label6.TabIndex = 4;
+            label6.Text = "Correo:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Palatino Linotype", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(443, 232);
+            label7.Name = "label7";
+            label7.Size = new Size(81, 18);
+            label7.TabIndex = 5;
+            label7.Text = "Contraseña:";
+            // 
+            // txtCorreo
+            // 
+            txtCorreo.Location = new Point(447, 188);
+            txtCorreo.Name = "txtCorreo";
+            txtCorreo.Size = new Size(265, 23);
+            txtCorreo.TabIndex = 6;
+            // 
+            // txtPass
+            // 
+            txtPass.Location = new Point(447, 268);
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(265, 23);
+            txtPass.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("MV Boli", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(744, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(44, 35);
+            button1.TabIndex = 8;
+            button1.Text = "X";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(255, 255, 192);
+            btnLogin.Font = new Font("Palatino Linotype", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogin.Location = new Point(521, 322);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(111, 38);
+            btnLogin.TabIndex = 9;
+            btnLogin.Text = "Confirmar";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnLogin);
+            Controls.Add(button1);
+            Controls.Add(txtPass);
+            Controls.Add(txtCorreo);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
             Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";
             panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -100,5 +220,15 @@
         private GroupBox groupBox1;
         private PictureBox pictureBox1;
         private Label label1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox txtCorreo;
+        private TextBox txtPass;
+        private Button button1;
+        private Button btnLogin;
     }
 }
--- BibliotecaSP/Login.cs
+++ BibliotecaSP/Login.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BLL.Servicios;
 
 namespace BibliotecaSP
 {
     public partial class Login : Form
     {
+        private ServicioUsuarios servicioUsuarios;
         public Login()
         {
             InitializeComponent();
+            this.servicioUsuarios = new ServicioUsuarios();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("¿Desea salir del sistema?", "Saliendo...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes) Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCorreo.Text) || string.IsNullOrEmpty(txtPass.Text)) 
+            {
+                MessageBox.Show("Debe llenar todos los campos");
+            }
+            else
+            {
+                var correo = this.txtCorreo.Text;
+                var clave = this.txtPass.Text;
+                var response = servicioUsuarios.Autenticar(correo,clave);
+
+                switch (response)
+                {
+                    case -1:
+                        MessageBox.Show("Correo no encontrado");
+                        break;
+                    case 0:
+                        MessageBox.Show("Credenciales no validas");
+                        break;
+                    case 1:
+                        MessageBox.Show("Autenticado");
+                        break;
+                }
+            }
+                
         }
     }
 }
